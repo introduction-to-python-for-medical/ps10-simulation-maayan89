299a0b6b5041026bf16e304941549efcca6ba194
maayan89 <maayan089@gmail.com>
2025-01-22 21:45:45 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -9,11 +9,11 @@ def spread_fire(grid):
                 neighbors = []
                 if i > 0:
                     neighbors.append(grid[i-1][j])
-                if i < grid_size-1:
+                if i < grid_size - 1:
                     neighbors.append(grid[i+1][j])
                 if j > 0:
-                    neighbors.append(grid[i-1][j-1])
-                if j < grid_size-1:
+                    neighbors.append(grid[i][j-1])
+                if j < grid_size - 1:
                     neighbors.append(grid[i][j+1])
                 if 2 in neighbors:
                     update_grid[i][j] = 2
