0909c70891730f6551cc4babcefac0984eac3338
maayan89 <maayan089@gmail.com>
2025-01-22 21:38:21 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -1,6 +1,5 @@
 import copy
 
-
 def spread_fire(grid):
     grid_size = len(grid)
     update_grid = copy.deepcopy(grid)
@@ -8,16 +7,15 @@ def spread_fire(grid):
         for j in range(grid_size):
             if grid[i][j] == 1:
                 neighbors = []
-                if i>0:
+                if i > 0:
                     neighbors.append(grid[i-1][j])
-                if i < grid_size - 1:
+                if i < grid_size-1:
                     neighbors.append(grid[i+1][j])
-                if j>0:
+                if j > 0:
                     neighbors.append(grid[i-1][j-1])
-                if j < grid_size - 1:
+                if j < grid_size-1:
                     neighbors.append(grid[i+1][j-1])
                 if 2 in neighbors:
                     update_grid[i][j] = 2
-
     return update_grid
 
